354779a40d50add4b913e95d97b4408d19eb3500
Harrier55 <harrier55@yandex.ru>
2022-06-30 00:30:33 +0500

    Task 18

--- app/src/main/java/ru/gb/bufet/MainActivity.kt
+++ app/src/main/java/ru/gb/bufet/MainActivity.kt
@@ -4,17 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.gb.bufet.databinding.ActivityMainBinding
 import ru.gb.bufet.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     //shared preference
-    private val SP: String = resources.getString(R.string.sp_name)
-    val viewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
+//    private val SP: String = resources.getString(R.string.sp_name)
+//    val viewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_restoran,
+//                R.id.navigation_favorite,
+//                R.id.navigation_zakaz,
+//                R.id.navigation_profile
+//            )
+//        )
+//        setupActionBarWithNavController(navController,appBarConfiguration)
+//        navView.setupWithNavController(navController)
+
     }
 }
