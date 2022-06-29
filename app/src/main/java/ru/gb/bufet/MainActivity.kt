1f4d96c0452f7e189567ddbf7b9bf2cb495021c2
KVM-DEV <mastadji@gmail.com>
2022-06-29 17:48:20 +0300

    splash animation, fullscreen display, add font, and fix shared bug in main activity, navigation

--- app/src/main/java/ru/gb/bufet/MainActivity.kt
+++ app/src/main/java/ru/gb/bufet/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.gb.bufet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import ru.gb.bufet.databinding.ActivityMainBinding
@@ -9,12 +11,15 @@ import ru.gb.bufet.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    //shared preference
-    private val SP: String = resources.getString(R.string.sp_name)
     val viewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //night mode off
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        //no title, no action bar
+        Window.FEATURE_NO_TITLE
+        supportActionBar?.hide()
     }
 }
