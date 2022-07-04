f0d659c2ac98e82c9023cab90cf167b3a7ac09f6
ant <theant@mail.ru>
2022-07-04 14:56:55 +0400

    добавил в манифест необходимые разрешения добавил во фрагмент временное текстовое поле для контроля получения данных реализовал во фрагменте ресторанов синхронный метож получения записи Food из базы данных

--- app/src/main/java/ru/gb/bufet/view/RestaurantsFragment.kt
+++ app/src/main/java/ru/gb/bufet/view/RestaurantsFragment.kt
@@ -2,11 +2,22 @@ package ru.gb.bufet.view
 
 import ru.gb.bufet.databinding.FragmentRestaurantsBinding
 import ru.gb.bufet.model.data.BaseFragment
+import ru.gb.bufet.model.domain.FoodRepository
+import ru.gb.bufet.retrofit.RetrofitFoodRepoImpl
 
 class RestaurantsFragment : BaseFragment<FragmentRestaurantsBinding>(
-    FragmentRestaurantsBinding::inflate) {
+    FragmentRestaurantsBinding::inflate
+) {
 
-    override fun init(){
-        //some initialization
+    private val repo: FoodRepository = RetrofitFoodRepoImpl()
+
+    override fun init() {
+        Thread {
+            val tmpDat = repo.getAllFood()
+
+            requireActivity().runOnUiThread {
+                binding.testFoodTw.text = tmpDat.toString()
+            }
+        }.start()
     }
 }
