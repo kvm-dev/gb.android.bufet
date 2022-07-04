5729b4d0dfa673453e713044822b40ff036759a3
Harrier55 <harrier55@yandex.ru>
2022-07-04 19:54:47 +0500

    общая разиетка

--- app/src/main/java/ru/gb/bufet/view/ProfileFragment.kt
+++ app/src/main/java/ru/gb/bufet/view/ProfileFragment.kt
@@ -6,6 +6,8 @@ import ru.gb.bufet.model.data.BaseFragment
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(
     FragmentProfileBinding::inflate) {
 
+///вот так должен выглядеть фрагмент
+
     override fun init(){
         //some initialization
     }
