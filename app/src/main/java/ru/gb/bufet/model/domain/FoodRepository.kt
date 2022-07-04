70ce462b735df4b11898ec42a603ad424c885a33
KVM-DEV <mastadji@gmail.com>
2022-07-04 17:18:22 +0300

    some refactoring of retrofit methods

--- app/src/main/java/ru/gb/bufet/model/domain/FoodRepository.kt
+++
@@ -1,5 +0,0 @@
-package ru.gb.bufet.model.domain
-
-interface FoodRepository {
-    fun getAllFood(): List<Food>
-}
--- app/src/main/java/ru/gb/bufet/viewModel/MainViewModel.kt
+++ app/src/main/java/ru/gb/bufet/viewModel/MainViewModel.kt
@@ -1,7 +1,29 @@
 package ru.gb.bufet.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
+import ru.gb.bufet.model.responseData.FoodListResponse
+import ru.gb.bufet.model.retrofit.RetrofitClient
+import ru.gb.bufet.model.interfaces.API
 
 class  MainViewModel: ViewModel() {
+    //data
+    val foodListResponse: MutableLiveData<List<FoodListResponse>> = MutableLiveData()
+    val error: MutableLiveData<String> = MutableLiveData()
+
+    //methods
+    fun getFoodList() {
+        val requestData = RetrofitClient.RetrofitHelper.getInstance().create(API.GetFoodsAPI::class.java)
+        viewModelScope.async {
+            try{
+                foodListResponse.value = requestData.getFoods()
+            }
+            catch (e: Exception) {
+                error.value = e.toString()
+            }
+        }
+    }
 
 }
