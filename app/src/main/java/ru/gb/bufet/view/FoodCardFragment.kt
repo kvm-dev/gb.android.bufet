977de65af9023777dc4dcad02226d417b199b6eb
Harrier55 <harrier55@yandex.ru>
2022-07-29 01:16:24 +0500

    add binding

--- app/src/main/java/ru/gb/bufet/view/FoodCardFragment.kt
+++ app/src/main/java/ru/gb/bufet/view/FoodCardFragment.kt
@@ -1,7 +1,6 @@
 package ru.gb.bufet.view
 
 import ru.gb.bufet.databinding.FragmentFoodCardBinding
-import ru.gb.bufet.databinding.FragmentRestaurantBinding
 import ru.gb.bufet.model.data.BaseFragment
 
 class FoodCardFragment: BaseFragment<FragmentFoodCardBinding>(FragmentFoodCardBinding::inflate) {
