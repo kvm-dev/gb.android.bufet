225328d2de53349230737f6b9de25152811dd5f5
ant <theant@mail.ru>
2022-07-30 15:16:52 +0400

    начал писать поиск блюда по ключевому слову

--- app/src/main/java/ru/gb/bufet/view/MenuFragment.kt
+++ app/src/main/java/ru/gb/bufet/view/MenuFragment.kt
@@ -1,19 +1,24 @@
 package ru.gb.bufet.view
 
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isNotEmpty
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.gb.bufet.databinding.FragmentMenuBinding
 import ru.gb.bufet.model.adapters.MenuAdapter
 import ru.gb.bufet.model.data.BaseFragment
 import ru.gb.bufet.model.responseData.Restaurant
+import ru.gb.bufet.model.responseData.RestaurantFood
 
 class MenuFragment : BaseFragment<FragmentMenuBinding>(
     FragmentMenuBinding::inflate
 ) {
-    override fun init() {
 
+    override fun init() {
         val restModel = getCurrentRestOrDefault()
-        bindingHeader(restModel)
         loadRestaurantMenu(restModel)
+        bindingHeader(restModel)
+
     }
 
 
@@ -31,10 +36,52 @@ class MenuFragment : BaseFragment<FragmentMenuBinding>(
     }
 
     private fun loadRestaurantMenu(restModel: Restaurant) {
-        binding.fragmentMenuMenuRecycler.apply {
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-            adapter = restModel.restaurantFood?.let { MenuAdapter(it) }
+
+        binding.fragmentMenuMenuRecycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        if (binding.fragmentMenuSearchView.query.isEmpty()) {
+            val adapter = restModel.restaurantFood?.let {
+                MenuAdapter(it)
+            }
+            adapter!!.notifyDataSetChanged()
+            binding.fragmentMenuMenuRecycler.adapter = adapter
+        } else {
+            search(restModel)
         }
+    }
+
+    private fun search(restModel: Restaurant) {
+
+        binding.fragmentMenuSearchView.clearFocus()
+
+//        if (binding.fragmentMenuSearchView.query.isNotEmpty()) {
+
+        binding.fragmentMenuSearchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(searchText: String): Boolean {
+                val filteredList = ArrayList<RestaurantFood>()
+                for (restFood in restModel.restaurantFood!!) {
+                    if (restFood.name!!.lowercase().contains(searchText.lowercase()))
+                        filteredList.add(restFood)
+                }
+
+                val adapter = MenuAdapter(filteredList)
+                adapter.notifyDataSetChanged()
+                binding.fragmentMenuMenuRecycler.adapter = adapter
+
+                return true
+            }
+        })
+//        } else {
+//            val adapter = restModel.restaurantFood?.let {
+//                MenuAdapter(it)
+//            }
+//            adapter!!.notifyDataSetChanged()
+//            binding.fragmentMenuMenuRecycler.adapter = adapter
+//        }
     }
 }
