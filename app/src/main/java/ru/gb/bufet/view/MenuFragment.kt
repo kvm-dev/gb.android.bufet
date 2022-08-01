c6603fae6084d60691ebfb3cb7e8e9b9857f89dd
ant <theant@mail.ru>
2022-08-01 16:45:36 +0400

    поправил высоту view TabLayout в fragment_menu.xml переписал методы поиска и фильтрации списка еды в MenuFragment.kt

--- app/src/main/java/ru/gb/bufet/view/MenuFragment.kt
+++ app/src/main/java/ru/gb/bufet/view/MenuFragment.kt
@@ -2,6 +2,7 @@ package ru.gb.bufet.view
 
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayout
 import ru.gb.bufet.databinding.FragmentMenuBinding
 import ru.gb.bufet.model.adapters.MenuAdapter
 import ru.gb.bufet.model.data.BaseFragment
@@ -16,7 +17,10 @@ class MenuFragment : BaseFragment<FragmentMenuBinding>(
         val restModel = getCurrentRestOrDefault()
 
         bindingHeader(restModel)
+        bindMenuFilter(restModel)
         loadRestaurantMenu(restModel)
+
+        //todo сделать едуную точку вызова рендера с передачей результатов поиска и фильтрации
     }
 
 
@@ -52,20 +56,69 @@ class MenuFragment : BaseFragment<FragmentMenuBinding>(
             }
 
             override fun onQueryTextChange(searchText: String): Boolean {
-                searchByName(restModel, searchText)
+                renderFoodList(searchByName(restModel, searchText))
                 return true
             }
         })
     }
 
-    private fun searchByName(restModel: Restaurant, searchText: String) {
+    private fun searchByName(restModel: Restaurant, searchText: String): ArrayList<RestaurantFood> {
         val filteredList = ArrayList<RestaurantFood>()
         for (restFood in restModel.restaurantFood!!) {
             if (restFood.name!!.lowercase().contains(searchText.lowercase()))
                 filteredList.add(restFood)
         }
+        return filteredList
+    }
 
-        val adapter = MenuAdapter(filteredList)
+    private fun searchByType(restModel: Restaurant, searchType: String): ArrayList<RestaurantFood> {
+        val filteredList = ArrayList<RestaurantFood>()
+        if (searchType == "все") {
+            filteredList.addAll(restModel.restaurantFood!!)
+        } else {
+            for (restFood in restModel.restaurantFood!!) {
+                if (restFood.type!!.lowercase().contains(searchType.lowercase()))
+                    filteredList.add(restFood)
+            }
+        }
+        return filteredList
+    }
+
+    private fun bindMenuFilter(restModel: Restaurant) {
+        val filterTabs = mutableSetOf<String>()
+        for (i in 0..restModel.restaurantFood!!.lastIndex) {
+            filterTabs.add(restModel.restaurantFood[i].type.toString())
+        }
+        binding.fragmentMenuFilterFood.addTab(
+            binding.fragmentMenuFilterFood.newTab().setText("все")
+        )
+        filterTabs.forEach {
+            binding.fragmentMenuFilterFood.addTab(
+                binding.fragmentMenuFilterFood.newTab().setText(it)
+            )
+        }
+
+        binding.fragmentMenuFilterFood.addOnTabSelectedListener(object :
+            TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                searchByType(restModel, tab?.text.toString())
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+//    private fun renderFoodList(searchedArray: ArrayList<RestaurantFood>, typArray: ArrayList<RestaurantFood>) {
+//
+//        val filteredList = searchedArray.intersect(typArray.toSet())
+//        val adapter = MenuAdapter(filteredList.toList())
+//        adapter.notifyDataSetChanged()
+//        binding.fragmentMenuMenuRecycler.adapter = adapter
+//    }
+//
+    private fun renderFoodList(searchedArray: ArrayList<RestaurantFood>) {
+        val adapter = MenuAdapter(searchedArray)
         adapter.notifyDataSetChanged()
         binding.fragmentMenuMenuRecycler.adapter = adapter
     }
